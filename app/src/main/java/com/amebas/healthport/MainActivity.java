4bc1225c09977c79c09fe69c8ff59f4008b6792f
BranSeal <sealbran@gmail.com>
2018-08-31 12:23:08 -0400

    A small test

--- app/src/main/java/com/amebas/healthport/MainActivity.java
+++ app/src/main/java/com/amebas/healthport/MainActivity.java
@@ -2,12 +2,14 @@ package com.amebas.healthport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Yeet","Hi");
     }
 }
