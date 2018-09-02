0bfc3882c845fccbead838d32c6cdab4d4f8a92d
Anush Mattapalli <matanatr96@gmail.com>
2018-09-02 13:56:38 -0400

    fixed appcompat error on activities. USE APPCOMPAT STYLES

--- app/src/main/java/com/amebas/healthport/MainActivity.java
+++ app/src/main/java/com/amebas/healthport/MainActivity.java
@@ -1,13 +1,15 @@
 package com.amebas.healthport;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     // Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +19,14 @@ public class MainActivity extends Activity {
         TextView txt = findViewById(R.id.titleText);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/OpenSans-Regular.ttf");
         txt.setTypeface(font);
+    }
+
+    public void login(View view) {
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
+    }
+
+    public void register(View view) {
+
     }
 }
