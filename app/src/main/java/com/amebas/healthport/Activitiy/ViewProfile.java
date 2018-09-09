68479d70ec443d804dca71b1dac77abf1d6dddd7
Anush Mattapalli <matanatr96@gmail.com>
2018-09-09 17:09:51 -0400

    added view profile page. needs to be cleaned up and designed but it works and shows text

--- app/src/main/java/com/amebas/healthport/Activitiy/ViewProfile.java
+++ app/src/main/java/com/amebas/healthport/Activitiy/ViewProfile.java
@@ -3,6 +3,13 @@ package com.amebas.healthport.Activitiy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.amebas.healthport.Model.Profile;
+import com.amebas.healthport.Model.SessionManager;
+import com.amebas.healthport.R;
+
 
 public class ViewProfile extends AppCompatActivity {
 
@@ -10,9 +17,22 @@ public class ViewProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
+        populateText();
     }
 
-    public void returnToDashboard() {
+    /**
+     * This function populates the textviews on the page with pertinent information
+     */
+    public void populateText() {
+        SessionManager instance = SessionManager.getInstance();
+        Profile profile = instance.getCurrentProfile();
+        TextView nameContent = (TextView) findViewById(R.id.nameContent);
+        TextView dobContent = (TextView) findViewById(R.id.dobContent);
+        nameContent.setText(profile.getName());
+        dobContent.setText(profile.getDob());
+    }
+
+    public void returnToDashboard(View view) {
         Intent dashboardIntent = new Intent(this, AccountDashboardActivity.class);
         startActivity(dashboardIntent);
     }
