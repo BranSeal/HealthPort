e284ec212c2e4c0bca481badfe76710dce6c60c0
mclee3360 <mclee3360@gmail.com>
2018-09-24 22:24:17 -0400

    Add method to get tags

--- app/src/main/java/com/amebas/healthport/Activity/FilePreviewActivity.java
+++ app/src/main/java/com/amebas/healthport/Activity/FilePreviewActivity.java
@@ -2,22 +2,32 @@ package com.amebas.healthport.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.amebas.healthport.R;
 
+import java.util.Arrays;
+
 public class FilePreviewActivity extends AppCompatActivity
 {
+
+    private String tags = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filepreview);
 
+        // @TODO Replace this array with array of profile names
         String[] arraySpinner = new String[] {
                 "User 1", "User 2", "User 3", "User 4", "User 5"
         };
@@ -38,9 +48,20 @@ public class FilePreviewActivity extends AppCompatActivity
             @Override
             public void onNothingSelected(AdapterView<?> parent) { }
         });
+
+
     }
 
-
+    /**
+     * Gets a list of the tags that the user typed into the tag field.
+     *
+     * @return list of tags.
+     */
+    private String[] getTags()
+    {
+        String text = ((EditText) findViewById(R.id.tag_input)).getText().toString().trim();
+        return (text.length() > 0) ? text.split(" ") : new String[0];
+    }
 }
 
 
