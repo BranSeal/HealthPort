4245d1eb0183a3d5aa9fc63bc1f9cc728126fc33
bseal3 <bseal3@gatech.edu>
2018-09-29 16:08:11 -0400

    Added code to open dialogue for taking picture or file and executing those tasks.

--- app/src/main/java/com/amebas/healthport/Activity/AccountDashboardActivity.java
+++ app/src/main/java/com/amebas/healthport/Activity/AccountDashboardActivity.java
@@ -9,7 +9,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
--- app/src/main/java/com/amebas/healthport/Activity/FilePreviewActivity.java
+++ app/src/main/java/com/amebas/healthport/Activity/FilePreviewActivity.java
@@ -1,25 +1,48 @@
 package com.amebas.healthport.Activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.amebas.healthport.Model.Profile;
+import com.amebas.healthport.Model.SessionManager;
 import com.amebas.healthport.R;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 
 public class FilePreviewActivity extends AppCompatActivity
 {
 
     private String tags = "";
+    private AlertDialog.Builder builder;
+    private String mCurrentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private static final int FILE_SELECT_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,9 +55,11 @@ public class FilePreviewActivity extends AppCompatActivity
         Log.d("Image", photoPath);
 
         // @TODO Replace this array with array of profile names
-        String[] arraySpinner = new String[] {
-                "User 1", "User 2", "User 3", "User 4", "User 5"
-        };
+        // This code breaks because of a null pointer exception from Model classes.
+//        SessionManager instance = SessionManager.getInstance();
+//        List<Profile> profiles = instance.getSessionAccount().getProfiles();
+//        String[] arraySpinner = (String []) profiles.toArray();
+        String[] arraySpinner = {"User1", "User2", "User3"};
         Spinner s = findViewById(R.id.profile_select);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, arraySpinner);
@@ -53,7 +78,28 @@ public class FilePreviewActivity extends AppCompatActivity
             public void onNothingSelected(AdapterView<?> parent) { }
         });
 
+        String[] colors = {"Take picture", "Upload file"};
 
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose what to do:");
+        builder.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == 0) {
+                    dispatchTakePictureIntent();
+                } else if (which == 1) {
+                    dispatchChooseFileIntent();
+                }
+            }
+        });
+
+        Button add_button = (Button) findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder.show();
+            }
+        });
     }
 
     /**
@@ -66,6 +112,90 @@ public class FilePreviewActivity extends AppCompatActivity
         String text = ((EditText) findViewById(R.id.tag_input)).getText().toString().trim();
         return (text.length() > 0) ? text.split(" ") : new String[0];
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == REQUEST_TAKE_PHOTO) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // TODO: ADD FILE TO LIST OF PAGES
+
+            } else if (resultCode == RESULT_CANCELED) {
+                File deleteF = (mCurrentPhotoPath!= null) ? new File(mCurrentPhotoPath) : null;
+                boolean deleted = false;
+                if (deleteF != null)
+                    deleted = deleteF.delete();
+                Log.w("Delete Check", "Empty file: " + mCurrentPhotoPath + "Deleted: " + deleted);
+            }
+        }
+    }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private void dispatchChooseFileIntent() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        //intent.setType("*/*");      //all files
+        intent.setType("*/*");   //XML file only
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.account_dashboard_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_menu:
+                Intent profileIntent = new Intent(this, ViewProfile.class);
+                startActivity(profileIntent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 }
-
-
