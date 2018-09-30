71d4bedcb131d884c68e4ae578a4557a0f74fe8d
mclee3360 <mclee3360@gmail.com>
2018-09-29 20:48:00 -0400

    Add document create/update confirmation screen

---
+++ app/src/main/java/com/amebas/healthport/Activity/DocConfirmActivity.java
@@ -0,0 +1,63 @@
+package com.amebas.healthport.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.amebas.healthport.R;
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.File;
+
+public class DocConfirmActivity extends AppCompatActivity {
+
+    private PDFView pdfView;
+    private File doc;
+    private TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_doc);
+
+        pdfView = findViewById(R.id.pdfView);
+        title = findViewById(R.id.docname);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null)
+        {
+            doc = (File) extras.getSerializable("doc");
+            title.setText(doc.getName());
+            // Pass in intents whether document was created or updated. Changes title accordingly.
+            if (extras.getBoolean("isNew"))
+            {
+                ((TextView) findViewById(R.id.msg)).setText(R.string.confirm_add);
+            }
+        }
+        doc = new File("/sdcard/Download/Full-Time Resume.pdf");
+        title.setText(doc.getName());
+        if (doc != null)
+        {
+            pdfView.fromFile(doc)
+            .onError(t -> {
+                Log.d("ERROR", "Failed to load pdf");
+            })
+            .pages(0)
+            .load();
+        }
+    }
+
+    /**
+     * Returns to document dashboard.
+     *
+     * @param v  the view calling the function.
+     */
+    public void exit(View v)
+    {
+        // @TODO: go to dashboard.
+    }
+
+}
