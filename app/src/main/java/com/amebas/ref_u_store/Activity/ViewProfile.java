3d7cad5ee8afd061b97b334d01044eafc6a7df7e
mclee3360 <mclee3360@gmail.com>
2018-11-05 17:18:49 -0500

    Add UI elements for switching profiles

--- app/src/main/java/com/amebas/ref_u_store/Activity/ViewProfile.java
+++ app/src/main/java/com/amebas/ref_u_store/Activity/ViewProfile.java
@@ -1,9 +1,15 @@
 package com.amebas.ref_u_store.Activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.amebas.ref_u_store.Model.Account;
@@ -37,8 +43,56 @@ public class ViewProfile extends AppCompatActivity {
         emailContent.setText(account.getEmail());
     }
 
+    /**
+     * Goes to the account dashboard screen.
+     *
+     * @param view  the view that called the method.
+     */
     public void returnToDashboard(View view) {
         Intent dashboardIntent = new Intent(this, AccountDashboardActivity.class);
         startActivity(dashboardIntent);
+    }
+
+    /**
+     * Displays the menu.
+     *
+     * @param v  view that called the method.
+     */
+    public void showMenu(View v)
+    {
+        PopupMenu menu = new PopupMenu(this, v);
+        MenuInflater inflater = menu.getMenuInflater();
+        inflater.inflate(R.menu.profile_options, menu.getMenu());
+        menu.setOnMenuItemClickListener(menu_item ->
+        {
+            switch (menu_item.getItemId())
+            {
+                case R.id.profile_edit:
+                    editProfile();
+                    return true;
+                case R.id.profile_switch:
+                    switchProfile();
+                    return true;
+                default:
+                    return false;
+            }
+        });
+        menu.show();
+    }
+
+    /**
+     * Moves to the edit profile screen.
+     */
+    private void editProfile()
+    {
+
+    }
+
+    /**
+     * "Signs out" of a profile, returning to profile select screen.
+     */
+    private void switchProfile()
+    {
+
     }
 }
