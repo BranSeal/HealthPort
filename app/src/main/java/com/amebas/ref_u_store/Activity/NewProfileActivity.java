5e36b89ba2efa269ccf20afecd5b4667be6351fa
mclee3360 <mclee3360@gmail.com>
2018-11-05 19:06:06 -0500

    Add cancelation confirm for creating new profile

--- app/src/main/java/com/amebas/ref_u_store/Activity/NewProfileActivity.java
+++ app/src/main/java/com/amebas/ref_u_store/Activity/NewProfileActivity.java
@@ -8,9 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.amebas.ref_u_store.Model.BinaryAction;
 import com.amebas.ref_u_store.Model.Profile;
 import com.amebas.ref_u_store.Model.SessionManager;
 import com.amebas.ref_u_store.R;
+import com.amebas.ref_u_store.Utilities.GeneralUtilities;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,10 +26,25 @@ public class NewProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_profile);
     }
 
+    /**
+     * Returns to the profile select screen. If there are non-empty text input fields, asks
+     * for confirmation first.
+     *
+     * @param view  the view that called the method.
+     */
     public void goToProfileSelect(View view) {
-        Intent profileSelect = new Intent(this, ProfileSelectActivity.class);
-        startActivity(profileSelect);
+        Map<String, String> values = getFields();
+        for (String key: values.keySet())
+        {
+            if (values.get(key).length() > 0)
+            {
+                confirmCancel();
+                return;
+            }
+        }
+        goToSelect();
     }
+
     public void goToSelect(){
         Intent profileSelect = new Intent(this, ProfileSelectActivity.class);
         startActivity(profileSelect);
@@ -53,6 +70,13 @@ public class NewProfileActivity extends AppCompatActivity {
             snack.setAction(R.string.ok, v -> snack.dismiss());
             snack.show();
         }
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        // Does same actions as canceling profile creation.
+        goToProfileSelect(findViewById(R.id.cancel_link));
     }
 
     /**
@@ -85,4 +109,24 @@ public class NewProfileActivity extends AppCompatActivity {
         }
         return false;
     }
+
+    /**
+     * Confirms the cancellation of creating a new profile.
+     */
+    private void confirmCancel()
+    {
+        GeneralUtilities.askConfirmation(this, getString(R.string.cancel_confirm), new BinaryAction()
+        {
+            @Override
+            public void confirmAction()
+            {
+                goToSelect();
+            }
+
+            @Override
+            public void denyAction() {}
+        });
+    }
+
+
 }
